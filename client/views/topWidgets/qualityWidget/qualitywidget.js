517ad4e7849be29e925f9c29fe0821d2adacf62c
Francisco Gutierrez <fsalvador23@gmail.com>
2015-12-18 18:40:16 +0100

    Updated risk

--- client/views/topWidgets/qualityWidget/qualitywidget.js
+++ client/views/topWidgets/qualityWidget/qualitywidget.js
@@ -1,39 +1,39 @@
 Template.qualitywidget.helpers({
   quality: function() {
-    var quality = Math.round(Session.get("qualityValue")*100);
-    return parseInt(quality) || 0;
-  },
-  qualityText: function() {
     var quality = Session.get("qualityValue");
-    var text;
-    if (quality >= 0.0) text = "Very Poor";
-    if (quality >= 0.2) text = "Poor";
-    if (quality >= 0.4) text = "Fair";
-    if (quality >= 0.6) text = "Good";
-    if (quality >= 0.8) text = "Very Good";
-    return text;
-  },
-  qualityColor: function() {
-    var quality = Session.get("qualityValue");
-    if (quality >= 0.0) $(".quality-bubble > #svgCircle").css("stroke","#e74c3c");
-    if (quality >= 0.2) $(".quality-bubble > #svgCircle").css("stroke","#e67e22");
-    if (quality >= 0.4) $(".quality-bubble > #svgCircle").css("stroke","#f1c40f");
-    if (quality >= 0.6) $(".quality-bubble > #svgCircle").css("stroke","#27ae60");
-    if (quality >= 0.8) $(".quality-bubble > #svgCircle").css("stroke","#25a085");
-    return quality;
-  },
-  casesNumber: function(){
-    return "45"
-  },
-  casesText: function () {
-    var quality = Session.get("qualityValue");
-    var text = "Not trustworthy";
-    if (quality >= 0.0) text = "Not Trustworthy";
-    if (quality >= 0.2) text = "Not Trustworthy";
-    if (quality >= 0.5) text = "Not Trustworthy";
-    if (quality >= 0.6) text = "Trustworthy";
-    if (quality >= 0.8) text = "Trustworthy";
-    return text;
+    var text = "";
+    var casestext = "";
+    if (quality >= 0.0){
+      text = "Very Poor";
+      casestext = "Untrustworthy";
+      $(".quality-bubble > #svgCircle").css("stroke","#e74c3c");
+    }
+    if (quality >= 0.2){
+      text = "Poor";
+      casestext = "Untrustworthy";
+      $(".quality-bubble > #svgCircle").css("stroke","#e67e22");
+    }
+    if (quality >= 0.4) {
+      text = "Fair";
+      casestext = "Untrustworthy";
+      $(".quality-bubble > #svgCircle").css("stroke","#f1c40f");
+    }
+    if (quality >= 0.6) {
+      text = "Good";
+      casestext = "Trustworthy";
+      $(".quality-bubble > #svgCircle").css("stroke","#27ae60");
+    }
+    if (quality >= 0.8) {
+      text = "Very Good";
+      casestext = "Trustworthy";
+      $(".quality-bubble > #svgCircle").css("stroke","#25a085");
+    }
+    return {
+      "value": parseInt(Math.round(quality*100)),
+      "text" : text,
+      "casestext": casestext,
+      "cases":"45"
+    };
   }
 });
 
@@ -69,3 +69,14 @@ Template.qualitywidget.events({
     }
   }
 });
+
+Template.qualitywidget.rendered = function () {
+  setTimeout(function() {
+    var quality = Session.get("qualityValue");
+    if (quality >= 0.0) $(".quality-bubble > #svgCircle").css("stroke","#e74c3c");
+    if (quality >= 0.2) $(".quality-bubble > #svgCircle").css("stroke","#e67e22");
+    if (quality >= 0.4) $(".quality-bubble > #svgCircle").css("stroke","#f1c40f");
+    if (quality >= 0.6) $(".quality-bubble > #svgCircle").css("stroke","#27ae60");
+    if (quality >= 0.8) $(".quality-bubble > #svgCircle").css("stroke","#25a085");
+  },1300);
+};
